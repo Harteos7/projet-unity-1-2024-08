6381920252ffadbdcf9a6fdd2d23c58126c9a669
Harteos7 <113110274+Harteos7@users.noreply.github.com>
2024-08-21 13:47:42 +0200

    white scar (vroom)

--- Assets/Scripts/Deplacement.cs
+++ Assets/Scripts/Deplacement.cs
@@ -6,20 +6,41 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
+    public float speed = 10f;
     private Rigidbody rb;
 
     void Start()
     {
         // Récupère le composant Rigidbody attaché au GameObject
         rb = GetComponent<Rigidbody>();
+        
     }
 
     void FixedUpdate()
     {
-        // Récupération des entrées clavier
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        Debug.Log(transform.position.z);
+
+        // Initialisation des variables de mouvement
+        float moveHorizontal = 0f;
+        float moveVertical = 0f;
+
+        // Vérification des touches enfoncées
+        if (Input.GetKey(KeyCode.Z))
+        {
+            moveVertical = 1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveVertical = -1f;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            moveHorizontal = -1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveHorizontal = 1f;
+        }
 
         // Calcul du vecteur de mouvement
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
