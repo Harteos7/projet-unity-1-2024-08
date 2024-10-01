44aab71bc116c921092cfd9738ddeffeb15108d8
Harteos7 <113110274+Harteos7@users.noreply.github.com>
2024-10-01 17:16:20 +0200

    Shift sans navmesh

--- Assets/Scripts/DeplacementSphere.cs
+++ Assets/Scripts/DeplacementSphere.cs
@@ -9,16 +9,15 @@ using UnityEngine.AI;
 public class DeplacementSphere : MonoBehaviour
 {
     public float vitesse; // Vitesse de déplacement de la sphère
+    public float hauteur = 0.5f ; // Hauteur du personnage 
     private Vector3 destination; // Destination vers laquelle la sphère se déplace
+    public bool seDeplacer;
+    public List<Vector3> Deplacements;
     private bool Deuxiemeclik = false; // Indique si le deuxième clic a été effectué
     public float raycastDistance = 100f; // Distance du raycast pour la visualisation
     NavMeshAgent agent;
     public LineRenderer lineRenderer;
-    public float pathUpdateInterval = 0.1f;
-    public GameObject previsuInstance;
-    public GameObject previsu;
-    public bool seDeplacer;
-    public List<Vector3> Deplacements;
+
 
     void Start()
     {
@@ -38,15 +37,14 @@ public class DeplacementSphere : MonoBehaviour
         if (Deplacements.Count > 0 && seDeplacer)
         {
             // Mettre à jour la destination si le personage est trops loin
-            if (Vector3.Distance(destination, transform.position) > 0.5f)
+            if (Vector3.Distance(Deplacements[0], transform.position) > 0.5f)
             {
                 agent.SetDestination(Deplacements[0]); // Définir la destination pour le NavMeshAgent
             }
             else
             {
-                seDeplacer = false;
                 Deplacements.RemoveAt(0); // suprimer la destination atteinte
-                if (previsuInstance != null) { Destroy(previsuInstance); } // Détruire la prévisualisation lorsque la destination est atteinte
+                if (Deplacements.Count == 0) {seDeplacer = false;} // plus de déplacements à faire ? si oui on s'arrête
             }
         }
 
@@ -54,11 +52,7 @@ public class DeplacementSphere : MonoBehaviour
 
         // Debug
         if (Input.GetKeyDown(KeyCode.E)) {
-            foreach (var i in Deplacements) {
-                Debug.Log(i);
-                Debug.Log(Deplacements.Count);
-            }
-
+            Debug.Log(Deplacements.Count);
         }
     }
 
@@ -67,7 +61,7 @@ public class DeplacementSphere : MonoBehaviour
 
 
     void ActionClik() {
-        // Vérifier si le clic gauche de la souris a été effectué (Premier clic)
+        // Premier déplacement
         if (Input.GetMouseButtonDown(0) && !Deuxiemeclik && Deplacements.Count == 0)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -76,9 +70,6 @@ public class DeplacementSphere : MonoBehaviour
             // Dessiner le rayon pour la visualisation
             Debug.DrawRay(ray.origin, ray.direction * raycastDistance, Color.red, 2f);
 
-            // Détruire l'ancienne prévisualisation si elle existe
-            if (previsuInstance != null) { Destroy(previsuInstance); }
-
             // Effectuer un raycast pour déterminer où le clic a eu lieu
             if (Physics.Raycast(ray, out hit, raycastDistance))
             {
@@ -86,9 +77,8 @@ public class DeplacementSphere : MonoBehaviour
                 if (hit.collider.CompareTag("Ground"))
                 {
                     // Positionner la destination en utilisant la hauteur du sol + 0.5 pour la sphère
-                    float hauteurSol = hit.point.y;
-                    destination = new Vector3(hit.point.x, hauteurSol + 0.5f, hit.point.z);
-                    previsuInstance = Instantiate(previsu, destination, Quaternion.identity);
+                    float hauteurSol = hit.point.y + hauteur;
+                    destination = new Vector3(hit.point.x, hauteurSol, hit.point.z);
 
                     // Calculer le chemin vers la destination sans déplacer l'agent
                     NavMeshPath path = new NavMeshPath();
@@ -105,47 +95,48 @@ public class DeplacementSphere : MonoBehaviour
             }
         }
 
-        // // Ajouter des déplacements
-        // else if (Input.GetMouseButtonDown(0) && !Deuxiemeclik && Input.GetKey(KeyCode.LeftShift)) {
-        //     Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        //     RaycastHit hit;
+        // Ajouter des déplacements grâce au bouton Shift
+        else if (Input.GetMouseButtonDown(0) && Deuxiemeclik && Input.GetKey(KeyCode.LeftShift)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
 
-        //     // Dessiner le rayon pour la visualisation
-        //     Debug.DrawRay(ray.origin, ray.direction * raycastDistance, Color.red, 2f);
+            // Dessiner le rayon pour la visualisation
+            Debug.DrawRay(ray.origin, ray.direction * raycastDistance, Color.red, 2f);
 
-        //     // Détruire l'ancienne prévisualisation si elle existe
-        //     if (previsuInstance != null) { Destroy(previsuInstance); }
+            // Effectuer un raycast pour déterminer où le clic a eu lieu
+            if (Physics.Raycast(ray, out hit, raycastDistance))
+            {
+                // Définir la nouvelle destination en fonction de la hauteur du sol détectée
+                if (hit.collider.CompareTag("Ground"))
+                {
+                    // Positionner la destination en utilisant la hauteur du sol + 0.5 pour la sphère
+                    float hauteurSol = hit.point.y + hauteur;
+                    destination = new Vector3(hit.point.x, hauteurSol, hit.point.z);
 
-        //     // Effectuer un raycast pour déterminer où le clic a eu lieu
-        //     if (Physics.Raycast(ray, out hit, raycastDistance))
-        //     {
-        //         // Définir la nouvelle destination en fonction de la hauteur du sol détectée
-        //         if (hit.collider.CompareTag("Ground"))
-        //         {
-        //             // Positionner la destination en utilisant la hauteur du sol + 0.5 pour la sphère
-        //             float hauteurSol = hit.point.y;
-        //             destination = new Vector3(hit.point.x, hauteurSol + 0.5f, hit.point.z);
-        //             previsuInstance = Instantiate(previsu, destination, Quaternion.identity);
+                    Deplacements.Add(destination); // Ajouter la destination à la liste
+                    lineRenderer.enabled = true; // Activer le LineRenderer pour montrer le chemin
 
-        //             // Calculer le chemin vers la destination sans déplacer l'agent
-        //             NavMeshPath path = new NavMeshPath();
-        //             if (agent.CalculatePath(destination, path))
-        //             {
-        //                 lineRenderer.enabled = true; // Activer le LineRenderer pour montrer le chemin
-        //                 lineRenderer.positionCount = path.corners.Length;
-        //                 lineRenderer.SetPositions(path.corners);
-        //             }
+                    Vector3[]positions = new Vector3[Deplacements.Count + 1];
 
-        //             Deplacements.Add(destination); // Ajouter la destination à la liste
-        //             Deuxiemeclik = true; // Marquer le premier clic comme terminé
-        //         }
-        //     }
-        // }
+                    positions[0] = agent.transform.position;
+
+                    for (int i = 0; i <Deplacements.Count; i++) {
+                        positions[i + 1] = Deplacements[i];
+                    }
+
+                    lineRenderer.positionCount = positions.Length;
+                    lineRenderer.SetPositions(positions);
+
+                    
+                    Deuxiemeclik = true; // Marquer le premier clic comme terminé
+                }
+            }
+        }
 
 
 
         // Vérifier si le deuxième clic gauche de la souris a été effectué
-        else if (Input.GetMouseButtonDown(0) && Deuxiemeclik)
+        else if (Input.GetMouseButtonDown(0) && Deuxiemeclik && !Input.GetKey(KeyCode.LeftShift))
         {
             seDeplacer = true; // on commence à se déplacer
             Deuxiemeclik = false; // Réinitialiser l'état pour les prochains clics
@@ -158,16 +149,14 @@ public class DeplacementSphere : MonoBehaviour
             Deuxiemeclik = false;
             Deplacements.Clear(); // on vide les positions obtenue
             seDeplacer = false; // on arréter le déplacement
-            if (previsuInstance != null) { Destroy(previsuInstance); }
             lineRenderer.enabled = false; // Désactiver le LineRenderer
         }
 
-        // On annule le déplacement
+        // On annule les déplacements
         if (Input.GetMouseButtonDown(1) && Deplacements.Count > 0) {
-            Deplacements.RemoveAt(0); // arrête de se déplacer
-            seDeplacer = false; // on arréter le déplacement
+            Deplacements.Clear(); // on supprime les déstinations
+            seDeplacer = false; // on arrête de se déplacer
             agent.ResetPath(); // on initialise la nouvelle position
-            if (previsuInstance != null) { Destroy(previsuInstance); }
         }
     }
 }
